674a545730ac8fc4e5f05f14c34e80b6ae8a7d88
Alexander Kromm <mmaulwurff@gmail.com>
2021-06-23 12:49:29 +0700

    update build script

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+filename=ultimate-custom-doom-$(git describe --abbrev=0 --tags).pk3
+
+mkdir -p build
+scripts/make_changelog.sh
+scripts/make_version.sh
+rm  -f "$filename"
+zip -R "$filename" "*.ogg" "*.png" "*.zs" "*.md" "*.txt"
+gzdoom "$filename" "$@" > output 2>&1; cat output
